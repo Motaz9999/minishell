3c3b2993ae2ef9f4233f3a0b7b3b7495d7a1a89d
Motaz9999 <motasjamal246@gmail.com>
2026-02-06 20:54:30 +0300

    first commit

---
+++ includes/minishell.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moodeh <moodeh@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/05 19:33:27 by moodeh            #+#    #+#             */
+/*   Updated: 2026/02/05 20:57:18 by moodeh           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include "struct.h"
+# include "libft.h"
+# include <stdio.h>
+# include <limits.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include <>
+# include <>
+# include <>
+# include <>
+
+
+#endif
