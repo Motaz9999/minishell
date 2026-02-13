b79c3e7fde2a789008685e0f629b551d82cbabb2
Motaz9999 <motasjamal246@gmail.com>
2026-02-13 22:14:21 +0300

    handle pipes

--- src/executor/pipes_handle.c
+++ src/executor/pipes_handle.c
@@ -6,7 +6,36 @@
 /*   By: moodeh <moodeh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/13 21:20:12 by moodeh            #+#    #+#             */
-/*   Updated: 2026/02/13 21:20:13 by moodeh           ###   ########.fr       */
+/*   Updated: 2026/02/13 22:14:01 by moodeh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minishell.h"
+// here we put the pipes then over ride it
+//return 1 if it success and 0 if it fail (the dub) and it also closes the pipes and anything else
+//here it just dup problem handle it inside the caller fun 
+int	handle_pipes(int prev_fd_in, int pipes[], int remaining_cmd , t_shell *shell)
+{
+	if (remaining_cmd > 1) // have more than 1 cmd (middle) or start//but how to know if it the first(easy by prev_fd_in)
+	{
+		if (prev_fd_in == -1)//first cmd
+		{
+			close(pipes[0]);
+			if (dup2(pipes[1] , 1) == -1)
+				return FALSE;
+		}
+		else// not the first cmd soo there is previous pipe i should take the input from it
+		{//but them in if statement
+			if (dup2(prev_fd_in,0) == -1 || dup2(pipes[1] , 1) == -1)//now i am taking from the prev pipe and close it in parent bc it live in it
+				return FALSE;
+		}
+	}
+	else if (remaining_cmd == 1) // the  last cmd  but what if 
+	{//how to check if 1 cmd or last cmd //use the same way 
+		if (prev_fd_in == -1)//first cmd  but in the same time last (just one cmd)
+			return TRUE;//there is nothing to do 
+		else // last cmd (dont have new pipe from parent) but have input comes from other cmds
+			dup2(prev_fd_in , 0);
+	}
+	return TRUE;
+}
