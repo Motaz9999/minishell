af0d296f19e90f0dd7e1020deede58e70de29d55
Motaz9999 <motasjamal246@gmail.com>
2026-04-11 22:46:34 +0300

    add header fixes

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: moodeh <moodeh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 19:33:27 by moodeh            #+#    #+#             */
-/*   Updated: 2026/03/30 20:01:28 by moodeh           ###   ########.fr       */
+/*   Updated: 2026/04/11 03:12:21 by moodeh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,7 +55,7 @@ int								count_commands(t_command *cmds);
 void							error_execve(char *cmd);
 int								error_cmd(char *cmd, char *msg, int exit_code);
 int								error_syscall(char *context, int exit_code);
-
+int								error_syntax(char *token, int exit_code);
 // redir + pipes handle
 int								handle_redir(t_redirect *redirections,
 									t_shell *shell);
@@ -64,6 +64,7 @@ int								handle_pipes(int p, int fds[], int rem,
 // signals
 void							setup_signals_child(void);
 void							setup_signals_parent(void);
+void							setup_heredoc(void);
 // builtins
 t_builtin						get_builtin(t_command *cmd);
 pid_t							execute_builtin(t_ext *ext, t_shell *shell,
@@ -97,10 +98,33 @@ char							*cut_value(char *arg, int cut);
 int								error_cmd_export(char *input);
 int								print_all_env_in_order(t_env *env_list);
 // parser / lexer
-t_token							*lexer(const char *line);
+t_token							*lexer(char *line);
+int							is_space(char c);
+int							is_operator_char(char c);
+t_token						*new_token(t_token_type type, char *value,
+										int quote_type);
+void							token_add_back(t_token **head, t_token *new_one);
+t_token						*read_operator_token(char *line, int *i);
+char							*read_word(char *line, int *i, int *quote_type);
+char							*read_plain_part(char *line, int *i);
+char							*read_single_quoted(char *line, int *i);
+char							*read_double_quoted(char *line, int *i);
 void							expand_tokens(t_token *tokens, t_shell *shell);
 t_command						*parser(t_token *tokens, t_shell *shell);
 void							parse_and_execute(char *line, t_shell *shell);
+int								validate_syntax(t_token *tokens);
+int								is_redir_token(t_token_type type);
+int								check_redir(t_token *tok);
+int								parse_redirection(t_command *cmd, t_token **tok,
+									t_shell *shell);
+int								count_args_in_segment(t_token *tok);
+t_command						*new_command(void);
+void							command_add_back(t_command **head,
+									t_command *new_one);
+t_redir_type					get_redir_type(t_token_type type);
+t_redirect						*new_redirect(t_redir_type type, char *file);
+void							redirect_add_back(t_redirect **head,
+									t_redirect *new_one);
 void							free_tokens(t_token *tokens);
 void							free_commands(t_command *commands);
 void							free_redirects(t_redirect *redirects);
@@ -129,6 +153,8 @@ void							execute_builtin_cmd(t_ext *ext, t_shell *shell);
 // expander
 void							expand_args_from_cmd(t_shell *shell,
 									t_command *cmd);
+char							*expand_cmd(char *expand_it, t_shell *shell);
+
 int								search_for_special(char *word);
 int								check_valid_key_char(char c);
 // replace str
